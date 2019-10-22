e1f0fcaac428bc7fdec67467c6f20387fdc5dabf
shabbir_ahmad52 <shabbir_ahmad52@yahoo.com>
2019-10-22 14:24:35 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { Admin, Resource } from "react-admin";
+import authProvider from "./authProvider";
+import  { UserList, UserEdit, UserCreate }  from './users';
+import jsonServerProvider from "ra-data-json-server";
+const dataProvider =
+  jsonServerProvider("https://jsonplaceholder.typicode.com");
+class App extends Component {
+  render() {
+    return (
+      <Admin dataProvider={dataProvider} authProvider={authProvider}>
+        <Resource name="users" list={UserList} edit={UserEdit} create={UserCreate} />
+      </Admin>
+    );
+  }
+}
+export default App;
